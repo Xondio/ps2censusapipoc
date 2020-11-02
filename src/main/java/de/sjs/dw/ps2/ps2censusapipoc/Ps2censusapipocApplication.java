c3f8ba2e496b1d2bf01ff20491770edbb3740130
Sebastian Schmidt <sebastian.schmidt@mailbox.org>
2020-11-02 14:19:25 +0100

    Klassen und Code bereinigt

--- src/main/java/de/sjs/dw/ps2/ps2censusapipoc/Ps2censusapipocApplication.java
+++ src/main/java/de/sjs/dw/ps2/ps2censusapipoc/Ps2censusapipocApplication.java
@@ -1,9 +1,7 @@
 package de.sjs.dw.ps2.ps2censusapipoc;
 
-import de.sjs.dw.ps2.api.OutfitListMembersAllFields;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -25,28 +23,4 @@ public class Ps2censusapipocApplication {
 		return builder.build();
 	}
 
-
-//	@Bean
-//	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-//		return args -> {
-//			Quote quote = restTemplate.getForObject(
-//					"https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-//			log.info(quote.toString());
-//		};
-//	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws  Exception{
-		return args -> {
-			OutfitListMembersAllFields outfitListMembersAllFields = restTemplate.getForObject(
-					"https://census.daybreakgames.com/get/ps2:v2/outfit/?name=DRUCKWELLE&c:resolve=member_character",
-					OutfitListMembersAllFields.class
-			);
-
-			//List<Member> members = outfitListMembersAllFields.getMembers();
-			//log.info("DWHQ Members count " + members.size());
-
-
-		};
-	}
 }
