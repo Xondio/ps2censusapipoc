c737fa5a3c062882113da16bc66ae3828466ba1e
Sebastian Schmidt <sebastian.schmidt@mailbox.org>
2020-11-02 14:22:05 +0100

    Packages renamed

--- src/main/java/de/sjs/dw/ps2/ps2censusapipoc/Ps2ApiService.java
+++ src/main/java/de/sjs/dw/ps2/ps2censusapipoc/Ps2ApiService.java
@@ -1,6 +1,6 @@
 package de.sjs.dw.ps2.ps2censusapipoc;
 
-import de.sjs.dw.ps2.api.OutfitListMembersAllFields;
+import de.sjs.dw.ps2.apienteties.OutfitListMembersAllFields;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
