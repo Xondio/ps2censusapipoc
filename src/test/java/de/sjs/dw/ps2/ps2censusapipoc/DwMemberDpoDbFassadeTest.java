d554db681f6ede53c51a4203e865c88f2d483554
Xondio <sebastian.schmidt@mailbox.org>
2022-02-16 19:47:12 +0100

    Refactoring: Refine DwMemberDpoDbFassadeTest

--- src/test/java/de/sjs/dw/ps2/ps2censusapipoc/DwMemberDpoDbFassadeTest.java
+++ src/test/java/de/sjs/dw/ps2/ps2censusapipoc/DwMemberDpoDbFassadeTest.java
@@ -1,11 +1,15 @@
 package de.sjs.dw.ps2.ps2censusapipoc;
 
 import de.sjs.dw.ps2.db.DwMemberDbFassade;
+import de.sjs.dw.ps2.db.DwMemberRepository;
 import de.sjs.dw.ps2.db.entities.DwMemberDpo;
 import de.sjs.dw.ps2.db.entities.PlayerCharacter;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,9 +19,13 @@ class DwMemberDpoDbFassadeTest {
     @Autowired
     DwMemberDbFassade dwMemberDbFassade;
 
+    @Autowired
+    DwMemberRepository dwMemberRepository;
 
+    @DisplayName("Can one Member be saved in DB (H2)")
     @Test
     void saveOneDwMember() {
+        // Arrange
         DwMemberDpo dwMemberDpo1 = new DwMemberDpo();
 
         dwMemberDpo1.setForumsId("testid");
@@ -37,15 +45,18 @@ class DwMemberDpoDbFassadeTest {
         dwMemberDpo1.getPlayerCharacters().add(pc1);
         dwMemberDpo1.getPlayerCharacters().add(pc2);
 
-
+        // Act
         DwMemberDpo returnDwMemberDpo = dwMemberDbFassade.saveOneDwMember(dwMemberDpo1);
 
 
-
+        // Assert
         assertNotNull(returnDwMemberDpo);
         assertNotNull(returnDwMemberDpo.getId());
         System.out.println(returnDwMemberDpo.getId());
         assertTrue(returnDwMemberDpo.getId() > 0L);
+        Optional<DwMemberDpo> dwMemberById = dwMemberRepository.findById(returnDwMemberDpo.getId());
+        assertTrue(dwMemberById.isPresent());
+        assertEquals(dwMemberById.get().getForumsName(), dwMemberDpo1.getForumsName());
 
 
     }
